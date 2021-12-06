64d123d54660b5a4ca1ae84c1977825f9918d640
Mustafa-Mohamed-M <mustafa.mohamed@thejitu.com>
2021-12-06 15:40:05 +0300

    Updated login UI

--- frontend/usersystem/src/components/auth/login.jsx
+++ frontend/usersystem/src/components/auth/login.jsx
@@ -30,10 +30,6 @@ export default function SignIn() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
     setErrorMessage(null);
     setMessage('Validating...');
     setLoading(true);
