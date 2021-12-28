7eb369b61c8fb0d18b7d8677e28a386a049b76f2
Mustafa Mohamed <mustafa.mohamed@thejitu.com>
2021-12-28 12:34:36 +0300

    Cron job for sending SMS using africastalking

--- api/background/index.js
+++ api/background/index.js
@@ -1,8 +1,50 @@
 const express = require("express");
 const cors = require("cors");
 const emailRoutes = require('./routes/emailRoutes');
-// const adminAuth = require('./middleware/isAdmin');
-// const auth = require('./middleware/auth');
+const cron = require('node-cron');
+const sql = require('mssql');
+const dbConfig = require('./database/config');
+const AfricasTalking = require('africastalking');
+require('dotenv').config();
+
+//initialize the Africastalking
+const africastalking = AfricasTalking({
+	apiKey: process.env.AFRICAS_TALKING_API_KEY, 
+	username: process.env.AFRICAS_TALKING_USERNAME
+});
+
+//send SMSs every few moments
+cron.schedule('* * * * *', () => { //run every minute!
+	sql.connect(dbConfig).then(pool =>{
+		return pool.request()
+		.execute('getAllUsers');
+	}).then((result, err)=>{
+		if (err){
+			console.log(err);
+		}
+		else{
+			console.log(`Preparing to send SMS to ${result.recordset.length} users.`);
+			for (let i = 0; i < result.recordset.length; i++){
+
+				const user = result.recordset[i];
+				let {username, phone_number} = user;
+				phone_number = '+254724572377';
+				const message = `Hello ${username}. Remember to work on your project today.`;
+				console.log(`Sending SMS to ${phone_number}`);
+				africastalking.SMS.send({
+					to: phone_number,
+					message,
+				}).then((result, err)=>{
+					if (err) console.log(err)
+					else console.log(`Message sent successfully.`);
+				});
+			}	
+		}
+		
+	}).catch(err=>{
+		console.log(err);
+	})
+});
 
 const port = 5003;
 
